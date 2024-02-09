f2b89255f4591e3502cf65ca96e27b1449db3826
ArunKuriakose15 <arunkp1515@gmail.com>
2024-02-09 16:14:50 +0530

    login

--- controllers/userRouter.js
+++ controllers/userRouter.js
@@ -1,24 +1,45 @@
-const express=require("express")
-const router=express.Router()
-const bcrypt=require("bcryptjs")
+const express = require("express")
+const router = express.Router()
+const bcrypt = require("bcryptjs")
 const userModel = require("../model/userModel")
 
-hashedPasswordGenerator=async(pass)=>{
-    const salt=await bcrypt.genSalt(10)
-    return bcrypt.hash(pass,salt)
+hashedPasswordGenerator = async (pass) => {
+    const salt = await bcrypt.genSalt(10)
+    return bcrypt.hash(pass, salt)
 }
 
-router.post("/signup",async(req,res)=>{
-    let {data}={"data":req.body}
-    let password=data.userPassword
+router.post("/signup", async (req, res) => {
+    let { data } = { "data": req.body }
+    let password = data.userPassword
 
-    const hashedPassword=await hashedPasswordGenerator(password)
-    data.userPassword=hashedPassword
-    let user=new userModel(data)
-    let result=user.save()
+    const hashedPassword = await hashedPasswordGenerator(password)
+    data.userPassword = hashedPassword
+    let user = new userModel(data)
+    let result = user.save()
     res.json({
-        status:"success"
+        status: "success"
     })
 })
 
-module.exports=router
+router.post("/login", async (req, res) => {
+    let input = req.body
+    let eMail = req.body.email
+    let data = await userModel.findOne({ "email": eMail })
+    if (!data) {
+        return res.json({ status: "invalid user" })
+    } 
+    else {
+        let dbPassword=data.userPassword
+        let inputPassword=req.body.userPassword
+        const match=await bcrypt.compare(inputPassword,dbPassword)
+        if(!match){
+            return res.json({status:"invalid password"})
+        }
+        else{
+            return res.json({status:"success"})
+        }
+
+    }
+})
+
+module.exports = router
